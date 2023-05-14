8090b76cea4c44fda7f2b5d10eef94d8c1c39e62
Antonio Felix Glorioso Ceretti <antonioglorioso1@hotmail.com>
2023-05-14 19:38:24 -0300

    Conexion a base de datos

--- src/app/service/skill.service.ts
+++ src/app/service/skill.service.ts
@@ -1,15 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { getDB } from './getDB.service';
+import { Observable } from 'rxjs';
+import { Skill } from '../model/skill';
 
 @Injectable({
 	providedIn: 'root'
 })
-export class SkillService extends getDB {
-	constructor(
-		http:HttpClient
-	) { 
-		super(http)
-		this.apiUrl += "skills";
+export class SkillService {
+	url = 'http://localhost:8080/habilidad/';
+
+	constructor(private httpClient:HttpClient) {}
+
+	public get(): Observable<Skill[]>{
+		return this.httpClient.get<Skill[]>(this.url + "list");
+	}
+
+	public save(skill: Skill):Observable<any>{
+		return this.httpClient.post<any>(this.url + 'save', skill);
+	}
+
+	public delete(id: number):Observable<any>{
+		return this.httpClient.delete<any>(this.url + `delete/${id}`);
+	}
+
+	public edit(skill: Skill):Observable<any>{
+		return this.httpClient.put<any>(this.url + 'update', skill);
 	}
 }
